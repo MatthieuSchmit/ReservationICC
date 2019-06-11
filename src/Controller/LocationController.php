3d1ba5fb67e25f7760d92fd90cb846615da0f536
MatthieuSchmit <matth.sht@gmail.com>
2019-06-11 13:52:26 +0200

    add location

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Locality;
 use App\Entity\Location;
+use App\Form\LocationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LocationController extends AbstractController {
@@ -31,10 +34,42 @@ class LocationController extends AbstractController {
     }
 
     /**
-     * @Route("admin/location/add", name="create_location")
+     * @Route(
+     *     name="create_location",
+     *     path="/admin/location/add",
+     * )
      * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function add() {
-        return $this->render('location/add.html.twig', []);
+    public function add(Request $request) {
+        $form = $this->createForm(LocationType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $location = new Location();
+            $location->setCoverImage($data->getCoverImage());
+            $location->setDescription($data->getDescription());
+            $location->setSlug(str_replace(' ', '-', $data->getDesignation()));
+            $location->setAddress($data->getAddress());
+            $location->setDesignation($data->getDesignation());
+            $location->setPhone($data->getPhone());
+            $location->setWebsite($data->getWebsite());
+
+            $l = $this->getDoctrine()->getRepository(Locality::class)->findAll()[0];
+            $location->setLocality($l);
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+            return $this->redirectToRoute('location', ['slug' => $location->getSlug()]);
+        }
+
+        return $this->render('location/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/LocalityType.php
+++ src/Form/LocalityType.php
@@ -3,30 +3,24 @@
 namespace App\Form;
 
 use App\Entity\Locality;
-use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class LocationType extends AbstractType
+class LocalityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('designation')
-            ->add('address')
-            ->add('website')
-            ->add('phone')
-            ->add('coverImage')
-            ->add('description')
+            ->add('postalCode')
+            ->add('locality')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Location::class,
+            'data_class' => Locality::class,
         ]);
     }
 }
